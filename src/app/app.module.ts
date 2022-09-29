97c359f9c00691b8956428bffd2ffa9210fb2a7f
l-leponner <laurentleponner@gmail.com>
2022-09-29 15:26:02 +0200

    Creation PicShare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PicShareComponent } from './pic-share/pic-share.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PicShareComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pic-share/pic-share.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-share',
+  templateUrl: './pic-share.component.html',
+  styleUrls: ['./pic-share.component.scss']
+})
+export class PicShareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
