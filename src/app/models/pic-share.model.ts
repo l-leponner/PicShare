876dd73e33c1ce78c5555d85f6faec6468f0bf6d
l-leponner <laurentleponner@gmail.com>
2022-10-03 17:54:19 +0200

    Addition PicShare class + ngIf + ngFor

---
+++ src/app/models/pic-share.model.ts
@@ -0,0 +1,10 @@
+export class PicShare{
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  imageUrl!: string;
+  likes!: number;
+  location?: string;
+
+}
--- src/app/pic-share/pic-share.component.ts
+++ src/app/pic-share/pic-share.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {PicShare} from "../models/pic-share.model";
 
 @Component({
   selector: 'app-pic-share',
   templateUrl: './pic-share.component.html',
   styleUrls: ['./pic-share.component.scss']
 })
-export class PicShareComponent implements OnInit {
+export class PicShareComponent implements OnInit{
+  @Input() picShare!: PicShare;
 
-  constructor() { }
+  buttonText!: string;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.buttonText = 'Me Likey!';
   }
 
+  onLike(){
+
+    if (this.buttonText === 'Me Likey!'){
+      this.picShare.likes++;
+      this.buttonText = 'Oops! Unlike?'
+    } else {
+      this.picShare.likes--;
+      this.buttonText = 'Me Likey!';
+    }
+  }
 }
