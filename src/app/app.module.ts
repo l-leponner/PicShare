e8da1480d6dd4d00e6a31e0fdfb364532c18e6aa
l-leponner <laurentleponner@gmail.com>
2022-10-05 16:46:40 +0200

    Change of locale to fr-FR + number modifying pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
 import { PicShareComponent } from './pic-share/pic-share.component';
 
@@ -12,7 +13,13 @@ import { PicShareComponent } from './pic-share/pic-share.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
